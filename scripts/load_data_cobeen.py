4e1b7d1895ff650555b0d90362b7fe46a297c5cf
William Sepesi <wsepesi@Williams-MacBook-Air.local>
2023-03-20 18:00:33 +0100

    auth added and packages refactored to string labels

--- scripts/load_data_cobeen.py
+++ scripts/load_data_cobeen.py
@@ -2,16 +2,20 @@ import pandas as pd
 from util import *
 
 def run():
-    collection = get_cobeen()
+    collection = get_collection('cobeen')
+    collection.delete_many({})
 
     # read in data from data/cobeen.csv
-    df = pd.read_csv('data/cobeen.csv')
+    # df = pd.read_csv('data/cobeen.csv')
+    df = pd.read_excel('data/cobeen.xlsx', dtype=str)
 
     # clean column names of internal whitespace, eg "First Name" -> "First_Name"
     df.columns = df.columns.str.replace(' ', '_')
 
     # drop na
     df = df.dropna()
+
+    # print(df.head())
 
     # load df to collection
     collection.insert_many(df.to_dict('records'))
