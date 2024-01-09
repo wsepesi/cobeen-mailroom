b00a24a1dfb47ad906a0bfec526abbcab15963f0
wsepesi <sepesi.w@wustl.edu>
2024-01-08 20:22:12 -0600

    a few more details

--- pages/report.tsx
+++ pages/report.tsx
@@ -78,10 +78,40 @@ const HALL = "Cobeen"
 const cardCn = "h-[78vh] overflow-auto"
 const INTERVAL = "Fall 2023"
 
+const getNumStudents = (data: DashboardLogged[]): number => {
+    const students = new Set<string>()
+    data.forEach((d) => {
+        students.add(d.studentId)
+    })
+    return students.size
+}
+
+const getNumDailyAvgPkgs = (data: DashboardLogged[]): number => {
+    const startDate = new Date("2023-08-01")
+    const endDate = new Date("2023-12-31")
+    const days = (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24)
+    return data.length / days
+}
+
+const getMedianTimeToPickup = (data: DashboardLogged[]): number => {
+    const times = data.map((d) => {
+        const ingested = new Date(d.ingestedTime)
+        const retrieved = new Date(d.retrievedTime)
+        const diff = retrieved.getTime() - ingested.getTime()
+        // calculate in hours
+        return diff / (1000 * 3600)
+    })
+    times.sort((a, b) => a - b)
+    const medianIndex = Math.floor(times.length / 2)
+    return times[medianIndex]
+}
+
 export default function Report() {
     const [isLoading, setIsLoading] = useState(false)
     const [data, setData] = useState<HallStats[] | null>(null)
     const [loggedData, setLoggedData] = useState<HallLogged[] | null>(null)
+
+    const loggedAndRestricted = restrictToInterval(loggedData?.map((d) => d.packages).flat() ?? [], INTERVAL)
     
     useEffect(() => {
         setIsLoading(true)
@@ -133,8 +163,11 @@ export default function Report() {
                             <CardContent className="my-3">
                                 <div>
                                     <div className="flex flex-row items-center justify-start">
-                                        <TypographyP className="mr-2"><strong>Total Packages:</strong></TypographyP>
-                                        <TypographyP className="">{(loggedData.map((d) => d.packages).flat()).length.toString()}</TypographyP>
+                                        <TypographyP className="mr-2">
+                                            A total of <strong>{loggedAndRestricted.length.toString()}</strong> packages were delivered to <strong>{getNumStudents(loggedAndRestricted).toString()}</strong> students.
+                                            On average, <strong>{getNumDailyAvgPkgs(loggedAndRestricted).toFixed(0)}</strong> packages were picked up per day. 
+                                            The median time to pickup for a package was <strong>{getMedianTimeToPickup(loggedAndRestricted).toFixed(1)}</strong> hours.
+                                        </TypographyP>
                                     </div>
                                     {/* <div>
                                     <ResponsiveContainer width={350} height={350}>
@@ -163,7 +196,7 @@ export default function Report() {
                                     halls={[lower(HALL)]}
                                 />
                                 <PackagesByStudentBarChart
-                                    data={loggedData.map((d) => d.packages).flat()}
+                                    data={loggedAndRestricted}
                                 />  
                             </CardContent>
                             </Card>
@@ -176,7 +209,7 @@ export default function Report() {
                             </CardHeader>
                             <CardContent className="space-y-2">
                                 <DataTable 
-                                    data={restrictToInterval(loggedData.map((d) => d.packages).flat(), INTERVAL)}
+                                    data={loggedAndRestricted}
                                     columns={columns}
                                 />
                             </CardContent>
